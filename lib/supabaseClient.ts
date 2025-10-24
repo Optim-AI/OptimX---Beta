f515c1c2cc7ad5289da45623cd7204be3960ddd8
Shaam S <shaamsarath7@gmail.com>
2025-10-24 12:29:32 +0530

    sqlite has been removed

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -5,3 +5,6 @@ export const supabase = createClient(
   'https://lkrvwszeveupyqebxehq.supabase.co',         // e.g., https://xyz.supabase.co
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrcnZ3c3pldmV1cHlxZWJ4ZWhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NjEzMzEsImV4cCI6MjA3MjAzNzMzMX0.OHBB4AXCQSksIvBov3obN_hSKyyuo4nyRtAyOv0dTC0'       // found in your Supabase API settings
 );
+
+
+// This is the file where things are going to be imported
