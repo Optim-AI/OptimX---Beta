4a4366099eb0508d7ceb656f22abfb0dd98b4356
Shaam S <shaamsarath7@gmail.com>
2025-12-06 10:15:02 +0530

    legacy browser change

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ const nextConfig = {
   experimental: {
     // Ensures pages/api works in production when app/ also exists
     manualClientBasePath: true,
-    legacyBrowsers: false,
     optimizePackageImports: ['react', 'next'],
   },
 };
